ad5305f561f9b49071e316f563006d91decfcfd5
Christopher Small <metasoarous@gmail.com>
2013-04-21 14:51:29 -0700

    Inital commit

---
+++ R/source_local.R
@@ -0,0 +1,5 @@
+source_local <- function(fname){
+    argv <- commandArgs(trailingOnly = FALSE)
+    base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
+    source(paste(base_dir, fname, sep="/"))
+}
